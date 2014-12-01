7f71bb5332b93c437e4d6a2ae6e4ec7a8ee92a52
aalgahmi <aalgahmi@gmail.com>
2014-12-01 14:34:32 -0700

    Made it easy to changed AR fonts.

--- bin/cssflip.sh
+++ bin/cssflip.sh
@@ -10,13 +10,7 @@ cp $3 "app/assets/stylesheets/$1/tmp.1.css"
 sed 's/f105/f104/g' app/assets/stylesheets/$1/tmp.1.css.rtl > app/assets/stylesheets/$1/tmp.2.css.rtl
 
 rm -f app/assets/stylesheets/$1/$2_rtl.css
-cat > "app/assets/stylesheets/$1/$2_rtl.css" <<EOF
-/*
-@import "overrides_rtl";
-@import "$2";
-*/
-EOF
 
-cat "app/assets/stylesheets/$1/tmp.2.css.rtl" >> "app/assets/stylesheets/$1/$2_rtl.css"
+cat "app/assets/stylesheets/$1/tmp.2.css.rtl" > "app/assets/stylesheets/$1/$2_rtl.css"
 
 rm -f app/assets/stylesheets/$1/tmp.*
