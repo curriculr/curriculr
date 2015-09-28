84992a5ece3ac5bd5b744cc16beba27a6a05a9ca
curriculr <admin@curriculr.org>
2015-09-28 08:07:26 -0600

    Added foreign keys

--- db/migrate/20150926141827_add_payment_to_klasses.payment.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from payment (originally 20150123152958)
-class AddPaymentToKlasses < ActiveRecord::Migration
-  def change
-    add_reference :klasses, :payment_plan, index: true
-    add_column :klasses, :payment_for_mask, :integer, default: 1
-  end
-end
